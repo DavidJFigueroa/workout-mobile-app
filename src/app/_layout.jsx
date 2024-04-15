debec661b929f06e1ca0d1a52b520037c9c0ec8b
David <user@David-M3s-MacBook-Pro.local>
2024-04-16 01:29:42 +0200

    query graphQL api using react query

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,14 +1,22 @@
 import {Stack} from "expo-router";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: "Exercises",
-        }}
-      />
-    </Stack>
+    <QueryClientProvider client={client}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Exercises",
+          }}
+        />
+      </Stack>
+    </QueryClientProvider>
   );
 }
