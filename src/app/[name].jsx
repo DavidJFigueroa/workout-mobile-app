27f07d2dcf52ea586e5adcd00bb3008b809cdc9b
David <user@David-M3s-MacBook-Pro.local>
2024-04-16 00:19:20 +0200

    GraphQL Api with Stepzen setup

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -2,9 +2,12 @@ import {View, Text, StyleSheet, ScrollView} from "react-native";
 import {useLocalSearchParams} from "expo-router";
 import exercises from "../../assets/data/exercises.json";
 import {Stack} from "expo-router";
+import {useState} from "react";
 
 export default function ExerciseDetailsScreen() {
   const params = useLocalSearchParams();
+
+  const [isInstructionExpanded, setIsInstructionExpanded] = useState(false);
 
   const exercise = exercises.find((item) => item.name === params.name);
 
@@ -17,13 +20,22 @@ export default function ExerciseDetailsScreen() {
       <View style={styles.panel}>
         <Text style={styles.exerciseName}>{exercise.name}</Text>
         <Text style={styles.exerciseSubtitle}>
-          <Text style={styles.subValue}>{exercise.muscle}|</Text>
+          <Text style={styles.subValue}>{exercise.muscle} | </Text>
           <Text style={styles.subValue}>{exercise.equipment}</Text>
         </Text>
       </View>
 
       <View style={styles.panel}>
-        <Text style={styles.instructions}>{exercise.instructions}</Text>
+        <Text
+          style={styles.instructions}
+          numberOfLines={isInstructionExpanded ? 0 : 3}>
+          {exercise.instructions}
+        </Text>
+        <Text
+          onPress={() => setIsInstructionExpanded(!isInstructionExpanded)}
+          style={styles.seeMore}>
+          {isInstructionExpanded ? "See less" : "See more"}
+        </Text>
       </View>
     </ScrollView>
   );
@@ -52,5 +64,11 @@ const styles = StyleSheet.create({
   instructions: {
     fontSize: 16,
     lineHeight: 20,
+  },
+  seeMore: {
+    alignSelf: "center",
+    padding: 10,
+    fontWeight: "600",
+    color: "grey",
   },
 });
